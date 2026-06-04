cf7221b9a306cefbba9891c31ba14fe927348306
markerupe-stack <markerupe225@gmail.com>
2026-06-04 16:15:51 +0300

    Create page.tsx

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#050816",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <h1 style={{ color: "#00E5FF" }}>
+        MRK.ER BOT
+      </h1>
+
+      <h2>Mark Erupe</h2>
+
+      <p>WhatsApp: 0757199240</p>
+    </main>
+  );
+}
